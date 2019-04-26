e3a9018cf2c66ea09a22c0d112c208f9c086d25b
Nadav Ivgi <nadav@shesek.info>
2019-04-26 21:49:48 +0300

    Add CLI options for server configuration

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,15 +2,39 @@
 
 extern crate asset_registry;
 extern crate stderrlog;
+#[macro_use]
+extern crate log;
 
-use std::path::Path;
+use std::path::PathBuf;
+
+use structopt::StructOpt;
 
 use asset_registry::errors::Result;
 use asset_registry::start_server;
 
+#[derive(StructOpt, Debug)]
+struct Config {
+    #[structopt(
+        short,
+        long,
+        parse(from_occurrences),
+        help = "Increase verbosity (up to 3)"
+    )]
+    verbose: usize,
+    #[structopt(short, long = "db-path", help = "Path to database directory")]
+    db_path: PathBuf,
+}
+
 fn main() -> Result<()> {
-    stderrlog::new().verbosity(3).init().unwrap();
-    // TODO make path configurable
-    start_server(&Path::new("./db"))?.launch();
+    let config = Config::from_args();
+    stderrlog::new()
+        .verbosity(config.verbose + 2)
+        .init()
+        .unwrap();
+    info!("Server config: {:?}", config);
+
+    start_server(&config.db_path)?.launch();
+    info!("HTTP server started");
+
     Ok(())
 }
